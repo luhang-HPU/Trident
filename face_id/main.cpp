0515d611f405a37a130be206176f231489776485
SunShuo <ss_0926@126.com>
2025-12-29 15:11:16 +0800

    feat: transform

--- face_id/main.cpp
+++ face_id/main.cpp
@@ -3,24 +3,11 @@
 #include "local_test/test.h"
 
 int main(int argc, char** argv) {
-#ifdef POSEIDON_USE_ZLIB
-    std::cerr << "zlib not support" << std::endl;
-    return -1;
-    std::cout << "macro POSEIDON_USE_ZLIB: on" << std::endl;
-#endif
-#ifdef POSEIDON_USE_ZSTD
-    std::cerr << "zstd not support" << std::endl;
-    return -1;
-    std::cout << "macro POSEIDON_USE_ZSTD: on" << std::endl;
-#endif
 
-    poseidon::PoseidonFactory::get_instance()->set_device_type(poseidon::DEVICE_SOFTWARE);
+
+    poseidon::PoseidonFactory::get_instance()->set_device_type(poseidon::DEVICE_HARDWARE);
 
     facial_recognition::BackendServer::getInstance().run();
-
-// DEBUG code
-//    facial_recognition::Test t;
-//    t.test_open_frontend_and_backend();
 
     return 0;
 }
