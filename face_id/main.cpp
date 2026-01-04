ea5a98331265973b153718501098fecde87b379c
tangjiajian <1308768831@qq.com>
2026-01-04 15:38:33 +0800

    fix: zlib & zstd

--- face_id/main.cpp
+++ face_id/main.cpp
@@ -3,6 +3,16 @@
 #include "local_test/test.h"
 
 int main(int argc, char** argv) {
+#ifdef POSEIDON_USE_ZLIB
+    std::cerr << "zlib not support" << std::endl;
+    return -1;
+    std::cout << "macro POSEIDON_USE_ZLIB: on" << std::endl;
+#endif
+#ifdef POSEIDON_USE_ZSTD
+    std::cerr << "zstd not support" << std::endl;
+    return -1;
+    std::cout << "macro POSEIDON_USE_ZSTD: on" << std::endl;
+#endif
 
 
     poseidon::PoseidonFactory::get_instance()->set_device_type(poseidon::DEVICE_HARDWARE);
